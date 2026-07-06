545bcd52f854fd47c303bf739906349139c4564f
mahmoud-mannes <personalmahmoudman@gmail.com>
2026-07-06 15:05:23 -0700

    Fix minor issues with function parameters, add k as a parameter to get_top_candidates function allowing choice of how many top candidates get returned

--- project_code/src/SAE_feature_analysis/top_candidates.py
+++ project_code/src/SAE_feature_analysis/top_candidates.py
@@ -57,16 +57,19 @@ def activation_extraction(model, processor, source, layer, number_images, RPI = 
     
 
 
-def get_top_candidates(latent_activations, target_position, num_tokens_per_image = 197):
+def get_top_candidates(latent_activations, target_position, num_tokens_per_image = 197, k=20):
+    """ Analyse features and their mean activations to determine top candidate features
+    responsible for spatial structure
+    """
     # NOTE: pos 0 represents the CLS token, therefore it is not included in our analyses of positional features
     num_tokens = latent_activations.shape[0]
 
-    mask = torch.arange(num_tokens, device = latent_activations.device) % 197 == target_position # this mask will allow us to select tokens at the target position
+    mask = torch.arange(num_tokens, device = latent_activations.device) % num_tokens_per_image == target_position # this mask will allow us to select tokens at the target position
 
     mean_activation_target_position = latent_activations[mask].mean(0)
     mean_activation_elsewhere = latent_activations[~mask].mean(0)
 
     selectivity_score = mean_activation_target_position - mean_activation_elsewhere
-    top_candidates = torch.topk(selectivity_score, k=20)
+    top_candidates = torch.topk(selectivity_score, k=k)
 
     return top_candidates
